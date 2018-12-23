0e56c1012462ef51aa196a2e7301d6b2e265b859
Anani1994 <dongwhchn@163.com>
2018-12-23 12:07:45 +0800

    配置支持 Less 语法

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -22,6 +22,16 @@ module.exports = {
       }]
     },
     {
+      test: /\.less$/,
+      use: [{
+        loader: 'style-loader' // creates style nodes from JS strings
+      }, {
+        loader: 'css-loader' // translates CSS into CommonJS
+      }, {
+        loader: 'less-loader' // compiles Less to CSS
+      }]
+    },
+    {
       test: /\.m?js$/,
       exclude: /node_modules/,
       use: {
