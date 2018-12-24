beff06e9feda1c36026294220d5f48f60408bad6
Anani1994 <dongwhchn@163.com>
2018-12-25 00:02:12 +0800

    配置 Redux 管理数据

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import store from './store';
 
-export default () => (
-  <div>
-    <div>home page</div>
-    <Link to="/product">go</Link>
-  </div>
-);
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = store.getState();
+    const { text } = this.state;
+    this.text = text;
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.text}</div>
+        <Link to="/product">go</Link>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/store/reducers.js
@@ -0,0 +1,6 @@
+const defaultState = {
+  text: 'Hello world',
+};
+
+/* 必须返回一个纯函数 */
+export default (state = defaultState) => state;
